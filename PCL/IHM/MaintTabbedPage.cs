d733fef6d6d694688b6c9db2c5792854df8496f7
Maxime Esprit <maxime.esprit@gmail.com>
2017-09-19 20:13:27 +0200

    rename

---
+++ PCL/IHM/MaintTabbedPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ESCL
+{
+	public class MainTabbedPage : TabbedPage 
+	{
+        readonly SettingsPage m_pageSettings;
+        readonly WebPage m_pageWebView;
+
+        public MainTabbedPage()
+        {
+            m_pageSettings = new SettingsPage { Title = "Paramètres" };
+            m_pageWebView = new WebPage { Title = "Données de consommation" };
+
+            Children.Add(m_pageSettings);
+            Children.Add(m_pageWebView);
+        }
+
+        public void SwitchToTabSetting()
+        {
+            CurrentPage = m_pageSettings;
+        }
+
+        public void SwitchToTabWebView()
+        {
+            m_pageWebView.refresh();
+            CurrentPage = m_pageWebView;
+        }
+	}
+}
+
---
+++ PCL/IHM/WebPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ESCL
+{
+	public class WebPage : ContentPage
+	{
+        public string m_sIPAdresse { get; set; }
+
+        private WebView m_webview;
+
+		public WebPage ()
+		{
+            m_webview = new WebView();
+            refresh();
+		}
+
+        public void refresh()
+        {
+            if (Settings.IPAdresse != "")
+            {
+                m_webview.Source = "http://" + Settings.IPAdresse + ":" + Settings.Port + "/projetERDF/";
+
+                Content = m_webview;
+            }
+        }
+	}
+}
+
